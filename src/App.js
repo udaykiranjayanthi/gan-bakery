4ac2c34f9edca82fb11a41128cfb57fddd5c8a12
Uday <udaykiranjayanthi099@gmail.com>
2022-05-16 11:25:07 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 import MyCart from './pages/MyCart';
 import Navigationbar from './components/Navigationbar';
+import Footer from './components/Footer';
 import store from './store/store'
 import { Provider } from 'react-redux'
 
@@ -15,17 +16,18 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <BrowserRouter>
-          <div className="min-h-screen">
             <Navigationbar />
-            <Routes>
-              <Route path="/" >
-                <Route index element={<Home />} />
-                <Route path="favorites" element={<Favorites />} />
-                <Route path="mycart" element={<MyCart />} />
+            <div className="min-h-screen">
+              <Routes>
+                <Route path="/" >
+                  <Route index element={<Home />} />
+                  <Route path="favorites" element={<Favorites />} />
+                  <Route path="mycart" element={<MyCart />} />
 
-              </Route>
-            </Routes>
-          </div>
+                </Route>
+              </Routes>
+            </div>
+            <Footer/>
         </BrowserRouter>
 
       </div>
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import { Container } from "react-bootstrap";
+
+
+function Footer(){
+    return(
+        <div className="footer">
+            <Container >
+                <div>
+                <span>&copy; UDAY - GAN STUDIO</span>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Footer;
